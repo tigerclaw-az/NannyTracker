073783c2831a2aeb469a686ac900f54b03a4f763
Erik Waibel <ejwaibel@gmail.com>
2014-04-08 16:47:19 -0700

    Update config.php settings

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php
-define('HOSTNAME','127.0.0.1:8080');      //Hostname or IP address
 define('USINGSSL','https');              	// If using HTTPS set to https otherwise leave as http.
 define('LOGFILE','log.txt');
 
 // Database configuration
-define('DB_NAME', 'NannyTracker');
-define('DB_USER', 'user');
-define('DB_PASS', 'passwd');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'nannytracker');
+define('DB_USER', 'homer');
+define('DB_PASS', 'c2ltcHNvbnM=');
