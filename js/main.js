bd02c723dfef279e61f16deb761f5b29a96d3b30
Erik Waibel <ejwaibel@gmail.com>
2014-06-10 10:04:55 -0700

    Update requirejs config to include requirejs plugins correctly

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-require.config({
+requirejs.config({
 	baseUrl: 'js/lib',
+	waitSeconds: 30,
+
 	paths: {
 		app: '../app',
-		route: '../routes',
-		tpl: '../templates'
+		model: '../app/models',
+		route: '../app/routes',
+		template: '../../templates',
+		widget: '../app/widgets',
+		worker: '../app/workers',
+		root: '../..',
+
+		// requirejs plugins
+		text: 'requirejs-plugins/text',
+		tpl: 'requirejs-plugins/template',
+		image: 'requirejs-plugins/image'
+	},
+	shim: {
+
 	}
 });
 
