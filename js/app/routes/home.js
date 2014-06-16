c3e4459052ab46b49cc354f9b219ee5720f16cae
Erik Waibel <ejwaibel@gmail.com>
2014-06-16 14:09:47 -0700

    Move 'routes' folder to js/app

--- js/app/routes/home.js
+++ js/app/routes/home.js
@@ -1,12 +1,12 @@
 define([], function() {
 	Path.map("#!/home").to(function(){
 	}).enter(function() {
-		require(['text!tpl/hello.html'], function(tplHello) {
+		require(['tpl!template/hello.html'], function(tplHello) {
 			$('body').append(tplHello.apply({
 				name: 'Erik'
 			}));
-		})
+		});
 	}).exit(function() {
 		// Exit from route
-	});	
+	});
 });
