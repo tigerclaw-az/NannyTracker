995af666909e1aba89d921a3646a6a0c67209aac
Erik Waibel <ejwaibel+github@gmail.com>
2014-06-16 19:27:21 -0700

    Add loading of home template to #!/home route

--- js/app/routes/home.js
+++ js/app/routes/home.js
@@ -1,7 +1,9 @@
 define([], function() {
 	Path.map("#!/home").to(function(){
 	}).enter(function() {
-		
+		require(['tpl!template/home.html'], function(tpl) {
+			$('#main').append($(tpl.apply()));
+		});
 	}).exit(function() {
 		// Exit from route
 	});
