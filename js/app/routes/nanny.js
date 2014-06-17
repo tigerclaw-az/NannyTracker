67d4f7c36fb778dc63057f09ef68bd13f620ec92
Erik Waibel <ejwaibel+github@gmail.com>
2014-06-16 19:11:17 -0700

    Fix styling and layouts for nanny page

--- js/app/routes/nanny.js
+++ js/app/routes/nanny.js
@@ -2,9 +2,10 @@ define([], function() {
 	Path.map("#!/nanny").to(function(){
 	}).enter(function() {
 		require(['tpl!template/nanny.html'], function(tplNanny) {
-			$('#container').append($(tplNanny.apply()));
+			$('#main').append($(tplNanny.apply()));
 		});
 	}).exit(function() {
 		// Exit from route
+		$('#main').off().empty();
 	});
 });
