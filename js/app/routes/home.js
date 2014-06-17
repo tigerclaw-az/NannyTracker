fbb78455b785aee474d2274db3760de1843778d9
Erik Waibel <ejwaibel+github@gmail.com>
2014-06-16 18:57:53 -0700

    Remove test code from home.js

--- js/app/routes/home.js
+++ js/app/routes/home.js
@@ -1,11 +1,7 @@
 define([], function() {
 	Path.map("#!/home").to(function(){
 	}).enter(function() {
-		require(['tpl!template/hello.html'], function(tplHello) {
-			$('body').append(tplHello.apply({
-				name: 'Erik'
-			}));
-		});
+		
 	}).exit(function() {
 		// Exit from route
 	});
