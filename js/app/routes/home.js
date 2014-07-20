97fe17126f8c7d2e7317ff09383b1713318e480e
jake <jake.h.studio@gmail.com>
2014-07-20 15:45:44 -0700

    added functionality for switching out the captions
    the functionality is based on the index of the carrousel and an array
    that holds all the info

--- js/app/routes/home.js
+++ js/app/routes/home.js
@@ -9,3 +9,25 @@ define([], function() {
 		$('#main').off().empty();
 	});
 });
+
+$('body').on('slid.bs.carousel', '#homeSlider', function () {
+	var caption = $("#caption");
+	var heading = "heading";
+	var body = "body text";
+
+	var carouselData = $(this).data('bs.carousel');
+  	var currentIndex = carouselData.getActiveIndex();
+
+  	var content = [
+	  		{heading: "Join Now!", body: "The easies way to get started today is to click the Sign Up button on the upper right corner of your screen. We look forward to having you as a new member of our ever-growing community!", btn: "signup",},
+	  		{heading: "Get to know Us!", body: "Our community of Parents and Nannies creates such a great community we want to tell you all about it. Click the button below to learn more about Nanny Tracker.", btn: "about",},
+	  		{heading: "Having Trouble?", body: "If you're having trouble naviging through Nanny Tracker or finding the information you need to find, then all you need to do is visit our FAQ page.", btn: "faq",},
+  		];
+
+    caption.empty();
+    caption.append('\
+    	<h3>' + content[currentIndex].heading + '</h3><p>' + content[currentIndex].body + '</p><br>\
+		<a class="btn btn-primary" href="#!/' + content[currentIndex].btn + '">learn more</a>\
+	');
+
+});
