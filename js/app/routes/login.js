989e7197ecb12a54bf8bf63c75071093554931b4
jake <jake.h.studio@gmail.com>
2014-08-04 00:36:40 -0700

    added the forgot password functionality
    added the forgot password functionality

--- js/app/routes/login.js
+++ js/app/routes/login.js
@@ -4,8 +4,25 @@ define([], function() {
 		require(['tpl!template/login.html'], function(tpl) {
 			$('#main').append($(tpl.apply()));
 		});
+		$(document).ready(function(){
+			$('header').hide();
+			$('#main').css('background-color', 'rgba(0,0,0,0)');
+			$('footer').hide();
+		});
 	}).exit(function() {
 		// Exit from route
-		$('#main').off().empty();
+		$('header').show();
+		$('#main').off().empty().css('background-color', '#fff');
+		$('footer').show();
 	});
+});
+
+$('body').on('click', '#signupCheck', function(){
+    if($(this).is(':checked')){
+		$('#pass').fadeOut();
+		$('#loginBtn').empty().html('Reset').attr('href', '/sendReset');
+    }else{
+		$('#pass').fadeIn();
+		$('#loginBtn').empty().html('Login').attr('href', '#');
+    }
 });
