b9ced70c4c04ddf123b8e7ac4b83947fa46a995d
Logan Fetter <loganfetter62@gmail.com>
2014-08-13 21:13:08 -0700

    Created the Parents Page Dynamically

--- js/app/routes/parent.js
+++ js/app/routes/parent.js
@@ -1,11 +1,28 @@
 define([], function() {
 	Path.map("#!/parent").to(function(){
 	}).enter(function() {
-		require(['tpl!template/parent.html'], function(tpl) {
-			$('#main').append($(tpl.apply()));
+		require(['tpl!template/parent.html', 'moment' ], function(tpl) {
+			$('#main').append($(tpl.apply({
+				messages: [{
+					message: 'Hey!',
+					time: moment().subtract(3, 'days').format('lll')
+				}, {
+					message: 'Uh-oh',
+					time: moment().format('lll')
+				}],
+				completedActions: [{
+					action: 'Nap',
+					startTime: '1:00',
+					endTime: '5:00',
+					notes: 'The child slept well!'
+				}]
+
+			})));
+
 		});
 	}).exit(function() {
 		// Exit from route
 		$('#main').off().empty();
+
 	});
 });
