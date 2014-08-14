c30a99771aeddacbf3ae309978841ca6da2b23ee
Erik Waibel <ejwaibel+github@gmail.com>
2014-08-13 20:24:00 -0700

    Convert static HTML to hogan template
    Closes #50

--- js/app/routes/nanny.js
+++ js/app/routes/nanny.js
@@ -1,8 +1,51 @@
 define([], function() {
-	Path.map("#!/nanny").to(function(){
+	Path.map("#!/nanny").to(function() {
 	}).enter(function() {
-		require(['tpl!template/nanny.html'], function(tpl) {
-			$('#main').append($(tpl.apply()));
+		require(['tpl!template/nanny.html', 'moment'], function(tpl) {
+			$('#main').append($(tpl.apply({
+				generalNotes: 'Big blurb of text',
+				messages: [{
+					message: 'Hey!',
+					time: moment().subtract(3, 'days').format('lll')
+				}, {
+					message: 'Uh-oh',
+					time: moment().format('lll')
+				}],
+				childrenTabs: [{
+					'isActive?': true,
+					name: 'Brianna'
+				}, {
+					name: 'Iris'
+				}],
+				children: [{
+					'isActive?': true,
+					name: 'Brianna',
+					actions: [{
+						action: 'Take Bath'
+					}, {
+						action: 'Feed Dog'
+					}],
+					completedActions: [{
+						action: 'Take Bath',
+						time: moment().format('lll')
+					}]
+				}, {
+					name: 'Iris',
+					actions: [{
+						action: 'Change Diaper'
+					}, {
+						action: 'Dinner'
+					}, {
+						action: 'Bath'
+					}],
+					completedActions: [{
+						action: 'Change Diaper',
+						time: moment().format('lll'),
+						'isNote?': true,
+						note: 'Screamed entire time'
+					}]
+				}]
+			})));
 		});
 	}).exit(function() {
 		// Exit from route
