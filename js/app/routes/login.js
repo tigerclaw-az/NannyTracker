2d51276c00024bdfd2307a3b7fd85216fe177130
Erik Waibel <ejwaibel+github@gmail.com>
2014-08-13 18:59:05 -0700

    Fix reset password button and login styles

--- js/app/routes/login.js
+++ js/app/routes/login.js
@@ -3,26 +3,29 @@ define([], function() {
 	}).enter(function() {
 		require(['tpl!template/login.html'], function(tpl) {
 			$('#main').append($(tpl.apply()));
-		});
-		$(document).ready(function(){
+
 			$('header').hide();
-			$('#main').css('background-color', 'rgba(0,0,0,0)');
+			$('#main').addClass('transparent-background');
 			$('footer').hide();
+
+			$('#signupCheck').on('click', function() {
+				if ($(this).is(':checked')) {
+					$('#pass').hide();
+					$()
+					$('#loginBtn').addClass('hide');
+					$('#resetBtn').removeClass('hide');
+				} else {
+					$('#pass').show();
+					$('#loginBtn').removeClass('hide');
+					$('#resetBtn').addClass('hide');
+				}
+			});
 		});
 	}).exit(function() {
 		// Exit from route
 		$('header').show();
-		$('#main').off().empty().css('background-color', '#fff');
+		$('#main').off().empty().removeClass('transparent-background');
 		$('footer').show();
 	});
 });
 
-$('body').on('click', '#signupCheck', function(){
-    if($(this).is(':checked')){
-		$('#pass').fadeOut();
-		$('#loginBtn').empty().html('Reset').attr('href', '/sendReset');
-    }else{
-		$('#pass').fadeIn();
-		$('#loginBtn').empty().html('Login').attr('href', '#');
-    }
-});
