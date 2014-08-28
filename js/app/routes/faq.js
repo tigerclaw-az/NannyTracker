9ec2a0503f36f7cb56e34daf17b1e74e99d1ea93
Logan Fetter <loganfetter62@gmail.com>
2014-08-27 19:25:58 -0700

    Added Mustache templating to Faq.html
    Closes #65

--- js/app/routes/faq.js
+++ js/app/routes/faq.js
@@ -1,8 +1,28 @@
 define([], function() {
 	Path.map("#!/faq").to(function(){
 	}).enter(function() {
-		require(['tpl!template/faq.html'], function(tpl) {
-			$('#main').append($(tpl.apply()));
+		require(['tpl!template/faq.html', 'moment'], function(tpl) {
+			$('#main').append($(tpl.apply({
+				faqRow: [{
+					subject: 'Newcommers!',
+					description: 'blah blah blah blah',
+					numTopics: '37',
+					numReplies: '45',
+					recentPost: 'blah blah blah blah',
+				},{
+					subject: 'Newcommers!',
+					description: 'blah blah blah blah',
+					numTopics: '37',
+					numReplies: '45',
+					recentPost: 'blah blah blah blah',
+				},{
+					subject: 'Newcommers!',
+					description: 'blah blah blah blah',
+					numTopics: '37',
+					numReplies: '45',
+					recentPost: 'blah blah blah blah',
+				},],
+			})));
 		});
 	}).exit(function() {
 		// Exit from route
