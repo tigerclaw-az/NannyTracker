55fa577c93773d1f30d2586f4d1db856f536ecfa
Erik Waibel <ejwaibel+github@gmail.com>
2014-09-09 00:10:40 -0700

    Set logs folder in config.php

--- api/config.php
+++ api/config.php
@@ -7,3 +7,6 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'nannytracker');
 define('DB_USER', 'homer');
 define('DB_PASS', 'c2ltcHNvbnM=');
+
+ini_set("log_errors", 1);
+ini_set("error_log", "logs/php-error.log");
