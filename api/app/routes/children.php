e34799e666f1f2d86f88f803d83d4fde30f046c5
jake <jake.h.studio@gmail.com>
2014-09-10 13:59:46 -0700

    added stuff to children api
    added what i think this api will look like, need assist

--- api/app/routes/children.php
+++ api/app/routes/children.php
@@ -2,6 +2,23 @@
 
 require_once APP_LIB_PATH . '/AppResponse.php';
 
+
+// need help i think i did this right but i dont know where to put getting al children
 $app->group('/children', function() use ($app, $nannyDB) {
+
+	// find child with id
+	$app->get('/children/:id', function($id) {
+
+	});
+
+	// update child with id
+	$app->post('/children/:id', function($id) {
+
+	});
+
+	// delete child with id
+	$app->delete('/children/:id', function($id) {
+
+	});
 
 });
