dfcb422590c84e832871196f870b265e229aca72
Erik Waibel <ejwaibel+github@gmail.com>
2014-09-10 19:26:24 -0700

    Add routes for parent
    #58

--- api/app/routes/parents.php
+++ api/app/routes/parents.php
@@ -3,5 +3,13 @@
 require_once APP_LIB_PATH . '/AppResponse.php';
 
 $app->group('/parents', function() use ($app, $nannyDB) {
+	$app->group('/:id', function($id) use ($app) {
+		$app->get('', function() use ($app, $id) {
+			// TODO: Get parent information for given :id
+		});
 
+		$app->get('/children', function() use ($app, $id)) {
+			// TODO: Get children for given parent :id
+		});
+	});	
 });
