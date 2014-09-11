9b84984e3e4c9e543b9d5544f123e8df56977c0f
Erik Waibel <ejwaibel+github@gmail.com>
2014-09-10 19:26:33 -0700

    Add API routes for Nanny
    #57

--- api/app/routes/nannies.php
+++ api/app/routes/nannies.php
@@ -3,5 +3,7 @@
 require_once APP_LIB_PATH . '/AppResponse.php';
 
 $app->group('/nannies', function() use ($app, $nannyDB) {
-
+	$app->get('/:id', function() use ($app) {
+		// TODO: Get nanny data for :id
+	});
 });
