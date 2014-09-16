da122a63ddbe5924e7a47dcc4004df03ef8e3bc6
jake <jake.h.studio@gmail.com>
2014-09-16 00:02:42 -0700

    removed commented stuff
    removed commented stuff

--- api/app/routes/children.php
+++ api/app/routes/children.php
@@ -5,14 +5,6 @@ require_once APP_LIB_PATH . '/AppResponse.php';
 
 $app->group('/children', function() use ($app, $nannyDB) {
 
-	// $app->map('', function() {
-	// 	if ($app->isPost()) {
-	// 		// TODO: Add new child
-	// 	} else {
-	// 		// Get all children
-	// 	}
-	// })->via('GET', 'POST');
-
 	// find child with id
 	$app->map('/:id', function($id) use($app) {
 		if ($app->isPost()) {
